3295738c5041aa985b770b20a5c974ba10e146f1
Steve Larsen <stevelarsen01@gmail.com>
2016-11-21 13:18:57 -0600

    create database

--- create_database.sql
+++ create_database.sql
@@ -1,3 +1,13 @@
--- Database name
+-- Database: sigma
 
--- Document your create tables SQL here
+CREATE TABLE treats (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100) NOT NULL,
+	description TEXT,
+	pic VARCHAR(100)
+	);
+
+
+INSERT INTO treats (name, description, pic)
+VALUES ('Cupcake', 'A delicious cupcake', '/assets/cupcake.jpg'),
+('Donuts', 'Mmmm donuts', '/assets/donuts.jpg');
